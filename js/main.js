40329dc131f400e676bf475484ddde221632b960
Charlie Crisp <charliecrisp249@gmail.com>
2017-04-01 18:36:52 +0100

    added scrolling functionality in left hand navigator column

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 var myHeight = $(window).height();
-$('#pdf').height(myHeight - 5);
-$('#left').css('margin-top', (myHeight/2)-10);
-$('#right').css('margin-top', (myHeight/2)-10);
+$('#pdf').height(myHeight-5);
+$('#left, #right').css('margin-top', (myHeight/2) - 40);
+$('.arrow').css('height', myHeight);
+$('.navigator').css('max-height',myHeight);
+$('.navigator').css('height',myHeight);
+$('.row, .container-fluid').css('height', myHeight);
 $(window).on('resize', function() {
     var myHeight = $(window).height();
-    $('#pdf').height(myHeight);
-    $('#left').css('margin-top', (myHeight/2)-10);
-    $('#right').css('margin-top', (myHeight/2)-10);
+    $('#pdf').height(myHeight-5);
+    $('#left, #right').css('margin-top', (myHeight/2)-40);
+    $('.arrow').css('height', myHeight);
+    $('.navigator').css('max-height',myHeight);
+    $('.navigator').css('height',myHeight);
+    $('.row, .container-fluid').css('height', myHeight);
 })
 openModule("Computer Design");
